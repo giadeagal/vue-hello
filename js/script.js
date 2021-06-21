b5748b61ea687ca8db9e12b1b7cefa37695cd2e6
Gianluca <gl.galassini@gmail.com>
2021-06-21 15:57:55 +0200

    fine progetto molto chic e di buon gusto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+    el: '#app',
+    data: {
+      azzurri: 'Forza Azzurri!',
+      azzurriGif: "https://cdn.dribbble.com/users/954572/screenshots/10213569/media/7e3157ad9ad764d0a01c23114adf04a5.gif",
+      azzurriCoro: "audio/popopo.mp3"
+    }
+  })
